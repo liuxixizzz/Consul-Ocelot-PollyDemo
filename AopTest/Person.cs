b9a902f42b41fc44f6444ba0cc3cc6f6d8099563
Louis Liu <1254922080@qq.com>
2020-06-04 11:37:05 +0800

    AopTest

---
+++ AopTest/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AopTest
+{
+    public class Person
+    {
+        [CustomInterceptor]
+        public virtual void Say(string msg)
+        {
+            Console.WriteLine("service calling..." + msg);
+        }
+    }
+}
---
+++ AopTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using AspectCore.DynamicProxy;
+using System;
+
+namespace AopTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProxyGeneratorBuilder proxyGeneratorBuilder = new ProxyGeneratorBuilder();
+            using (IProxyGenerator proxyGenerator = proxyGeneratorBuilder.Build())
+            {
+                Person person = proxyGenerator.CreateClassProxy<Person>();
+                person.Say("say!");
+            }
+        }
+    }
+}
