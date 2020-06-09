16384607af1c718e5af37477286f81a9fc1396a2
Louis Liu <1254922080@qq.com>
2020-06-09 10:21:37 +0800

    添加HttpHelper

---
+++ MsgService/Units/JsonExtends.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MsgService.Units
+{
+    public static class JsonExtends
+    {
+        public static T ToEntity<T>(this string val)
+        {
+            return JsonConvert.DeserializeObject<T>(val);
+        }
+
+        //public static List<T> ToEntityList<T>(this string val)
+        //{
+        //    return JsonConvert.DeserializeObject<List<T>>(val);
+        //}
+        public static string ToJson<T>(this T entity, Formatting formatting = Formatting.None)
+        {
+            return JsonConvert.SerializeObject(entity, formatting);
+        }
+    }
+}
